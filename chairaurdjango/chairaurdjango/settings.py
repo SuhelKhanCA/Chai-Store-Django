d44732cec11d60635eec40a19d4cd6f6781df9ee
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-11-27 15:02:08 +0000

    npm path issues solve

--- chairaurdjango/chairaurdjango/settings.py
+++ chairaurdjango/chairaurdjango/settings.py
@@ -49,7 +49,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-NPM_BIN_PATH = r"C:\\Program Files\\nodejs\\npm.cmd"
+# NPM_BIN_PATH = r"C:\\Program Files\\nodejs\\npm.cmd" # for windows
+NPM_BIN_PATH = '/usr/local/bin/npm' # on local machine
+NPM_BIN_PATH = "/home/codespace/nvm/current/bin/npm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
