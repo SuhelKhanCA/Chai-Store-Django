d3c9ba32585a9b4772cd9e8eb55d8b40bcf7c336
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-11-27 14:08:49 +0000

    add routing in the chai app

--- chairaurdjango/chai/urls.py
+++ chairaurdjango/chai/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 # localhost:8000/chai
+# localhost:8000/chai/order
 urlpatterns = [
     path('', views.all_chai, name='All_chai'),
-    # path('', views.all_chai, name='order'),
+    # path('chai/order', views.all_chai, name='order'),
 ]
